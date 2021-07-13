1b6348ca113f45f253067ead831923053a70496a
xsaven <gms.sw.xsaven@gmail.com>
2021-07-13 20:37:44 +0300

    CHANGELOG.md, src/ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,30 +4,51 @@ namespace Bfg\BlessModel;
 
 use Bfg\BlessModel\ApplyLevy\SaveLevyModelCollectionListener;
 use Bfg\BlessModel\ApplyLevy\SaveLevyModelListener;
+use Bfg\Installer\Providers\InstalledProvider;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 
 /**
  * Class ServiceProvider
  * @package Bfg\BlessModel
  */
-class ServiceProvider extends IlluminateServiceProvider
+class ServiceProvider extends InstalledProvider
 {
     /**
-     * Bootstrap services.
-     * @return void
+     * The name of extension.
+     * @var string|null
      */
-    public function boot()
-    {
-
-    }
+    public ?string $name = "bfg/bless_model";
 
     /**
-     * Register route settings
+     * The description of extension.
+     * @var string|null
+     */
+    public ?string $description = "To quickly save data in the model";
+
+    /**
+     * Set as installed by default.
+     * @var bool
+     */
+    public bool $installed = true;
+
+    /**
+     * Executed when the provider is registered
+     * and the extension is installed.
      * @return void
      */
-    public function register()
+    function installed(): void
     {
         \Event::listen(LevyModel::class, SaveLevyModelListener::class);
         \Event::listen(LevyModelCollection::class, SaveLevyModelCollectionListener::class);
+    }
+
+    /**
+     * Executed when the provider run method
+     * "boot" and the extension is installed.
+     * @return void
+     */
+    function run(): void
+    {
+
     }
 }
